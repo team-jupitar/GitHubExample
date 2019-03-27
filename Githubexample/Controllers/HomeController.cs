2338bb9a4bc60d26e420b399d67a2cd7e41f7113
Mukesh-Arora <43337964+Mukesh-Arora@users.noreply.github.com>
2019-03-27 17:01:03 +0530

    release candidate

--- Githubexample/Controllers/HomeController.cs
+++ Githubexample/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace Githubexample.Controllers
-{
+{//Candidate release
     public class HomeController : Controller
     {
         public ActionResult Index()
