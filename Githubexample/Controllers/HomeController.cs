21fdba0275ba84147d68c80faa613b7ccfc4bbde
Mukesh-Arora <43337964+Mukesh-Arora@users.noreply.github.com>
2019-04-16 12:58:26 +0530

    commit

--- Githubexample/Controllers/HomeController.cs
+++ Githubexample/Controllers/HomeController.cs
@@ -13,7 +13,16 @@ namespace Githubexample.Controllers
             return View();
         }
 
+        public ActionResult maraprfeature2mthod3()
+        {
+            ViewBag.Message = "Your application april feature2method3 page.";
 
+            ViewBag.Message = "Your application april feature2method3 page.";
+            //comment view 1
+            //comment by mohit
+
+            return View();
+        }
         public ActionResult maraprfeaturemthod3()
         {
             ViewBag.Message = "Your application april feature3 page.";
