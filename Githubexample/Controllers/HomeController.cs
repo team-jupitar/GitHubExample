acc7e57c8b62839b4db94ec4383d3d5f85bc59b4
karan <karan@viennasolutions.com>
2019-04-19 12:51:47 +0530

    AprilTest

--- Githubexample/Controllers/HomeController.cs
+++ Githubexample/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace Githubexample.Controllers
         public ActionResult LoadData()
         {
            ViewData["Message"]="User ViewData";
+            ViewBag.canI="Don't Use It";
             return View();
         }
 
