361be191d362acb73b4a838546c2bc51d8ab5d65
mohit2389 <mohit@9785>
2019-04-23 15:59:49 +0530

    Donhe

--- Githubexample/Controllers/HomeController.cs
+++ Githubexample/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace Githubexample.Controllers
         public ActionResult LoadData()
         {
            ViewData["Message"]="User ViewData";
+            ViewData["Message"] = "User ViewData1";
             return View();
         }
 
