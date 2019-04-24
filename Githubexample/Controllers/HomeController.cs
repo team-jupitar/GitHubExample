5c1183effe4744339f66f3de33506c43ed933e69
karan <karan@viennasolutions.com>
2019-04-24 10:19:13 +0530

    Commit Not Working

--- Githubexample/Controllers/HomeController.cs
+++ Githubexample/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace Githubexample.Controllers
         public ActionResult TestApril()
             {
             ViewData["Merge"]="Merge";
-            ViewData["Check"] = "Checked";
+            ViewData["Check"] = "Checkeds";
             return View();
 }
 
