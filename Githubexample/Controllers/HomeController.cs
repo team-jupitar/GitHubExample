74358793320840da57f8847e3b99ebac8bb70e02
mohit2389 <mohit@9785>
2019-04-24 15:20:54 +0530

    Test

--- Githubexample/Controllers/HomeController.cs
+++ Githubexample/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace Githubexample.Controllers
         public ActionResult LoadData()
         {
            ViewData["Message"]="User ViewData";
-            ViewBag.canI="Don't Use It12";
+           ViewBag.canI="Don't Use It12";
             return View();
         }
 
