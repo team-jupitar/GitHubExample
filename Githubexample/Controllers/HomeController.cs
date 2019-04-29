7de41670d6a353766431411cb924b485a4277aa2
karanvienna <karan@viennasolutions.com>
2019-04-29 18:57:51 +0530

    LoadData3

--- Githubexample/Controllers/HomeController.cs
+++ Githubexample/Controllers/HomeController.cs
@@ -29,6 +29,32 @@ namespace Githubexample.Controllers
             return View();
 }
 
+
+        
+           public ActionResult LoadData2()
+        {
+           ViewData["Message"]="2";
+            ViewBag.canI="2";
+            TempData["Temp"]="2";
+
+            return View();
+        }
+
+
+            public ActionResult LoadData3()
+        {
+           ViewData["Message"]="3";
+            ViewBag.canI="3";
+            TempData["Temp"]="3";
+
+            return View();
+        }
+
+
+
+
+           
+
         public ActionResult maraprfeaturemthod3()
         {
             ViewBag.Message = "Your application april feature3 page.";
