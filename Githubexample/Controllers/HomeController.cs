d9e11c1c5ff6e65c2200e66067982ba764c6725f
mohit2389 <mohit@9785>
2019-05-02 13:04:39 +0530

    Comm

--- Githubexample/Controllers/HomeController.cs
+++ Githubexample/Controllers/HomeController.cs
@@ -46,16 +46,6 @@ namespace Githubexample.Controllers
         }
 
 
-        
-           public ActionResult LoadData2()
-        {
-           ViewData["Message"]="2";
-            ViewBag.canI="2";
-            TempData["Temp"]="2";
-
-            return View();
-        }
-
 
             public ActionResult LoadData3()
         {
